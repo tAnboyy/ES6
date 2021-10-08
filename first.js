651cbb761358dbb8cf381712e4cde723509de19c
Thanmay <mdasthanmay@gmail.com>
2021-10-08 20:03:51 +0530

    08-10-21

---
+++ first.js
@@ -0,0 +1,149 @@
+//let
+function greetPerson(name) {
+    let greet;
+    // let greet;
+    if (name === "tan") {
+        greet = "Hi tan";
+    } else {
+        greet = "Hello";
+    }
+    console.log(greet);
+    // let greet;
+}
+
+greetPerson("tan");
+
+//const VS let
+let num1;
+// const num2;
+const num2 = 10;
+// const num2 = 20;
+// num2 = 20;
+console.log(num2);
+const obj1 = {
+    name: "Tan"
+}
+obj1.age = 19;
+// obj1 = {
+//
+// }
+
+console.log(obj1);
+
+const PI = 3.14;
+let r = 3;
+let area = PI * r * r;
+
+//functions-
+//a
+function sal(x, y, z) {
+    return x * y + 10;
+}
+
+sal(6, 7, 8);
+//b
+const sal2 = function (x, y, z) {
+    return x * y + 10;
+};
+sal2(4, 5, 6)
+//c
+const sal3 = (x, y, z) => x * y + z;
+sal3(2, 3, 4)
+
+const sal4 = x => x;
+sal4(9);
+const sal5 = () => {
+    return 99;
+}
+sal5();
+
+//11
+let employee = {
+    id: 1,
+    greet: function (that) {
+        // const self = this;
+        // setTimeout(function(){console.log(self.id)}, 1000);
+        setTimeout(() => console.log(this.id), 1000);
+    }
+}
+employee.greet();
+
+//12
+let percentBonus = () => 0.1;
+let getValue = function (value = 10, bonus = value * percentBonus()) {
+    console.log(value + bonus);
+    console.log(arguments.length);
+}
+getValue();
+getValue(20);
+getValue(20, 30);
+getValue(undefined, 30);
+
+//13
+let displayColors = function (message, ...colors) {
+    console.log(message);
+    console.log(colors);
+    console.log(arguments.length);
+    // for(let i in arguments){
+    //     console.log(arguments[i]);
+    // }
+    for (let i in colors) {
+        console.log(colors[i]);
+    }
+}
+
+let message = "List of colors- ";
+displayColors(message, 'Red');
+displayColors(message, 'Red', 'Blue');
+displayColors(message, 'Red', 'Blue', 'Green');
+
+//14
+let displayColors2 = function (message, ...colors) {
+    console.log(message);
+
+    for (let i in colors) {
+        console.log(colors[i]);
+    }
+}
+
+let colorArray = ['Orange', 'Yellow', 'Indigo'];
+displayColors2(message, ...colorArray);
+
+//15
+let firstName = "Thanmay"
+let lastName = "Das"
+let es6obj = {
+    firstName, //firstName: firstName.. ie property, value same
+    lastName
+}
+console.log(es6obj.firstName)
+console.log(es6obj.lastName)
+
+function createPerson(firstName, lastName, age) {
+    let fullName = firstName + " " + lastName;
+    return {
+        firstName,
+        lastName,
+        fullName,
+        // isSenior: function(){}.. before es6
+        isSenior() {
+            return age > 65;
+        }
+    }
+}
+let p = createPerson("Matt", "Hardy", 69);
+console.log(p.firstName);
+console.log(p.lastName);
+console.log(p.fullName);
+console.log(p.isSenior());
+
+//16
+let variable = "last name";
+let obj2 = {
+    "first name": "Thanmay", //space in property name, accessible using [], not using .
+    [variable]: "M Das" //variable as property name
+}
+
+console.log(obj2);
+console.log(obj2["first name"]);
+console.log(obj2["last name"]);
